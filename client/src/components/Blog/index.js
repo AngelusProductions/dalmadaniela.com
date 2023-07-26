5330bb0727c494e015b701b0b4acb69e3e3771cd
AngelusProductons <angelusproductions@gmail.com>
2023-07-25 21:17:46 -0700

    cleanup TipTap

--- client/src/components/Blog/index.js
+++ client/src/components/Blog/index.js
@@ -1,80 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 
 import TipTap from '../UI/tiptap/TipTap'
 import {
-    BASIC_CONTENT,
-    MARKDOWN_SHORTCUTS_CONTENT,
-    POPOVER_EDITOR_CONTENT,
-    SYNTAX_HIGHLGHTING_CONTENT,
-    LONG_DOCUMENT_PERFORMANCE_CONTENT,
-    READ_ONLY_MODE_CONTENT,
-    READ_ONLY_MODE_V2_CONTENT,
+    BASIC_CONTENT
 } from '../UI/tiptap/data'
 import './index.scss'
 
-export const Blog = props => (
+export const Blog = props => {
+  const [content, setContent] = useState(BASIC_CONTENT)
+  return (
   <div id="Wrapper">
     <TipTap
-        content={BASIC_CONTENT}
-        withToolbar={true}
-        withTaskListExtension={true}
-        withLinkExtension={true}
-        withEmojisReplacer={true}
+        content={content}
+        withToolbar
+        withTaskListExtension
+        withLinkExtension
+        withEmojisReplacer
+        withPopover
+        withTypographyExtension
+        withHexColorsDecorator
+        withMentionSuggestion
+        withEmojiSuggestion
+        withCodeBlockLowlightExtension
+        withPlaceholderExtension
     />
-    <TipTap
-        content={POPOVER_EDITOR_CONTENT}
-        withPopover={true}
-        withLinkExtension={true}
-    />
-    <TipTap
-        content={MARKDOWN_SHORTCUTS_CONTENT}
-        withTypographyExtension={true}
-        withEmojisReplacer={true}
-        withHexColorsDecorator={true}
-    />
-    <TipTap
-        //content={SUGGESTIONS_EDITOR_CONTENT}
-        withMentionSuggestion={true}
-        withEmojiSuggestion={true}
-    />
-    <TipTap
-        content={SYNTAX_HIGHLGHTING_CONTENT}
-        withCodeBlockLowlightExtension={true}
-    />
-    <TipTap withPlaceholderExtension={true} withEmojisReplacer={true} />
-    <TipTap content={LONG_DOCUMENT_PERFORMANCE_CONTENT} withToolbar={true} />
-    {[...Array(20)].map((_, key) => (
-        <TipTap
-            key={key}
-            content={READ_ONLY_MODE_CONTENT}
-            editable={false}
-            withTaskListExtension={true}
-            withLinkExtension={true}
-            withEmojisReplacer={true}
-            withTypographyExtension={true}
-            withHexColorsDecorator={true}
-            withMentionSuggestion={true}
-            withEmojiSuggestion={true}
-            withCodeBlockLowlightExtension={true}
-        />
-    ))}
-    {[...Array(300)].map((_, key) => (
-        <TipTap
-            key={key}
-            content={READ_ONLY_MODE_V2_CONTENT}
-            editable={false}
-            withTaskListExtension={true}
-            withLinkExtension={true}
-            withEmojisReplacer={true}
-            withTypographyExtension={true}
-            withHexColorsDecorator={true}
-            withMentionSuggestion={true}
-            withEmojiSuggestion={true}
-            withCodeBlockLowlightExtension={true}
-        />
-    ))}
   </div>
-)
+)}
 
 export default Blog
