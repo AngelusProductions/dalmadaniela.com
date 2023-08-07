368f835e554199701bc9e6aaa88cb0b39812d45a
AngelusProductons <angelusproductions@gmail.com>
2023-08-07 00:21:09 -0700

    Configure Server

--- client/src/actions/auth.js
+++
@@ -1,8 +0,0 @@
-import { UPDATE_FIELD_AUTH } from '../constants'
-
-export const changeAuthFieldText = (form, key, value) => ({
-  type: UPDATE_FIELD_AUTH,
-  form,
-  key,
-  value
-})
--- client/src/constants/data/admins.js
+++
@@ -1,8 +0,0 @@
-export const emailSignature = '@dalmadaniela.com'
-
-export const adminEmails = [
-    `dalma${emailSignature}`,
-    `camireyes${emailSignature}`,
-    `corey${emailSignature}`,
-    'angelusproductions@gmail.com'
-]
