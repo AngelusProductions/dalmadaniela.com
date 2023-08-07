43fd82444bc48c544e7dae8a4bcbfce1e99e40b4
AngelusProductons <angelusproductions@gmail.com>
2023-08-07 13:57:58 -0700

    Change Backend

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,7 +1,7 @@
 export const STRIPE_PUBLISHABLE_TEST_KEY = 'pk_live_'
     + '51N3WiQFYHcKM5vsb4WirmcQQvWZQ52JnLRQgiEmyCH9qy05a2Ju3AU8UfApZFz4CjnF8XVPZA8vCtft41D2VNiu000JBve8IAg'
 
-export const PRODUCTION_API_HOSTNAME = 'dalma-daniela-server.herokuapp.com'
+export const PRODUCTION_API_HOSTNAME = 'dalmadaniela-server-b88a618509c8.herokuapp.com'
 
 export const API_VERSION = 1
 
