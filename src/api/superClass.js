f4461ae7ef55118d51a61d52fe665dc01e81973c
AngelusProductons <angelusproductions@gmail.com>
2023-08-09 15:58:38 -0700

    Move Client Out to root

--- src/api/superClass.js
+++ src/api/superClass.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { ROOT_API_URL, apiEndpoints } from '../constants/paths'
 
 export const checkForReoccuringIP = async IP => {
-  const response = await axios.get(`${ROOT_API_URL}${apiEndpoints.superClass.checkIP}/${IP})}`)
+  const response = await axios.get(`${ROOT_API_URL}${apiEndpoints.superClass.checkIP}/${IP}`)
   debugger
   return response.data
 }
