bbd3b4482a5b7612aa6e62c82414f8d6b18a7705
AngelusProductons <angelusproductions@gmail.com>
2023-10-07 18:15:11 -0700

    Text Change

--- src/components/SuperClass/text.js
+++ src/components/SuperClass/text.js
@@ -6,19 +6,19 @@ export default {
     superClassFeatureBullets: [
         {
             title: 'No BS Promises.',
-            body: 'We don\'t sell virality. We sell a revenue-oriented system that increases credibility so the people who really matter find you without you looking for them.'
+            body: 'I don\'t sell virality. I sell a revenue-oriented system that increases credibility so the people who really matter find you without you looking for them.'
         },
         {
             title: 'A practical method for social media success!',
-            body: 'Monetize your social channels by relying on a proven strategy that\'s generated over 7 figures in revenue for English and Spanish-speaking businesses.'
+            body: 'Monetize your social channels by relying on a proven strategy that\'s generated over 7 figures in revenue for all of the businesses I\'ve worked for.'
         },
         {
             title: 'Increase your confidence by always knowing what to post.',
-            body: 'Are you an artist? Create purposefully authentic content that helps you achieve your goals in the real world on autopilot!'
+            body: 'Are you also an artist? Create purposefully authentic content that helps you achieve your goals on autopilot!'
         },
         {
             title: 'Money-back guarantee.',
-            body: 'We\'re so sure of our method that if you don\'t get results in 3 months, you\'ll get your money back. PS. We\'re not really worried about that ;)'
+            body: 'I\'m so sure of this strategy that if you don\'t get results in 3 months, you\'ll get your money back. PS. I\'m not really worried about that ;)'
         }
     ],
 
