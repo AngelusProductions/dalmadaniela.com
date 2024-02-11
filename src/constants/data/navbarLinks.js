3b209f3bca2d214d6f3777034f0f29fc327cd920
AngelusProductons <angelusproductions@gmail.com>
2024-02-11 14:55:54 -0800

    Link to ap.com

--- src/constants/data/navbarLinks.js
+++ src/constants/data/navbarLinks.js
@@ -3,51 +3,51 @@ import { paths } from '../../constants/paths'
 export default [
   {
     id: 1,
-    name: 'Home',
+    name: "Home",
     path: paths.home,
-    side: 'left',
-    tooltip: null
+    side: "left",
+    tooltip: null,
   },
   {
     id: 2,
-    name: 'SuperClass',
+    name: "SuperClass",
     path: paths.superClass.page,
-    side: 'left',
-    tooltip: null
+    side: "left",
+    tooltip: null,
   },
   {
     id: 3,
-    name: 'Magic calendars',
+    name: "Magic calendars",
     path: paths.magicCalendars.page,
-    side: 'left',
-    tooltip: null
+    side: "left",
+    tooltip: null,
   },
   {
     id: 4,
-    name: 'Blog',
+    name: "Blog",
     path: paths.blog.page,
-    side: 'left',
-    tooltip: null
+    side: "left",
+    tooltip: null,
   },
   {
     id: 5,
-    name: 'Meet the team',
+    name: "Meet the team",
     path: paths.team,
-    side: 'right',
-    tooltip: null
+    side: "right",
+    tooltip: null,
   },
   {
     id: 6,
-    name: 'Contact us',
+    name: "Contact us",
     path: paths.contactUs,
-    side: 'right',
-    tooltip: null
+    side: "right",
+    tooltip: null,
   },
   {
     id: 7,
-    name: 'Digital marketing services',
-    path: paths.services,
-    side: 'right',
-    tooltip: null
-  }
-]
+    name: "Angelus Productions",
+    path: "https://angelusproductions.com",
+    side: "right",
+    tooltip: null,
+  },
+];
